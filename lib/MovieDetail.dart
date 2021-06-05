65f7ca82761ebd937c37e7a27c0e7d73c66ff4d0
owenltm <owenlwiantoro01@gmail.com>
2021-06-05 22:21:38 +0800

    review error dialog

--- lib/MovieDetail.dart
+++ lib/MovieDetail.dart
@@ -91,6 +91,7 @@ class _MovieDetail extends State<MovieDetail>{
         genre = "Action/Thriller";
         sypnosis = "Mike Banning is framed for the attempted assasination of the President and must evade his own agency and the FBI as he tries to uncover the real threat";
     }
+
     return Scaffold(
       body: SingleChildScrollView(
               child: Column(
@@ -176,41 +177,51 @@ class _MovieDetail extends State<MovieDetail>{
               Container(
                   margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
                   child: TextFormField(
-                  controller: reviews,
-                  maxLines: 3,
-                  style: TextStyle(color: Colors.black, fontSize: 18),
-                  validator: (String val){
-                    if(val.length > 1000 && val.isNotEmpty){
-                      return "*Review must be less than equals 1000 characters";
-                    }
-                    else{
-                      cek = 1;
-                    }
-                    if(cek == 1){
-                      reviews.text = "";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    hintText: "Write a review",
-                    errorStyle: TextStyle(fontSize: 18),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: BorderSide(color: Colors.black, width: 3) 
+                    controller: reviews,
+                    maxLines: 3,
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                    validator: (String val){
+                      if(val.length > 1000 && val.isNotEmpty){
+                        showDialog(context: context, builder: (_) =>
+                           AlertDialog(
+                            title: Text(
+                              "Error",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            content: Text('Review must be less than equals 1000 characters'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, 'OK'),
+                                child: Text('OK'),
+                              ),
+                            ],
+                          )
+                        );
+                      } else {
+                        reviews.text = "";
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Write a review",
+                      errorStyle: TextStyle(fontSize: 18),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(color: Colors.black, width: 3) 
+                        ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(color: Colors.black, width: 3) 
                       ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: BorderSide(color: Colors.black, width: 3) 
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(color: Colors.black, width: 3) 
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(color: Colors.black, width: 3) 
+                      )
                     ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                       borderSide: BorderSide(color: Colors.black, width: 3) 
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide: BorderSide(color: Colors.black, width: 3) 
-                    )
                   ),
-                ),
                 ),
                 Row(
                   children: <Widget>[
